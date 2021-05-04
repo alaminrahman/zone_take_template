7943baf6b3f25e6a76b24df0d5d6d9a34163bfdd
Al-amin Rahman <alaminrahman>
2021-05-04 14:58:49 +0600

    Profile Complete

--- js/custom.js
+++ js/custom.js
@@ -22,3 +22,52 @@ $(document).ready(function(){
 
 
 })
+
+
+function readURL(input) {
+     if (input.files && input.files[0]) {
+   
+       var reader = new FileReader();
+   
+       reader.onload = function(e) {
+         $('.image-upload-wrap').hide();
+   
+         $('.file-upload-image').attr('src', e.target.result);
+         $('.file-upload-content').show();
+   
+         $('.image-title').html(input.files[0].name);
+       };
+   
+       reader.readAsDataURL(input.files[0]);
+   
+     } else {
+       removeUpload();
+     }
+   }
+   
+   function removeUpload() {
+     $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+     $('.file-upload-content').hide();
+     $('.image-upload-wrap').show();
+   }
+   $('.image-upload-wrap').bind('dragover', function () {
+       $('.image-upload-wrap').addClass('image-dropping');
+     });
+     $('.image-upload-wrap').bind('dragleave', function () {
+       $('.image-upload-wrap').removeClass('image-dropping');
+   });
+
+
+  // Start Profile Edit
+  $('#profile_edit').click(function(){
+    $(this).addClass('d-none');
+    $('#profile_update').removeClass('d-none');
+    $('#name').removeAttr('readonly');
+    $('#email').removeAttr('readonly');
+    $('#mobile').removeAttr('readonly');
+    $('#address').removeAttr('readonly');
+
+  })
+
+
+  // End Profile Edit
